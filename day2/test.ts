aad814d694cf51b1db697c4afd868590acc5b5d6
Sirajis Salekin Sajon <sirajis.sajon@selise.ch>
2023-12-02 15:05:35 +0600

    2 gold star for day 2

--- day2/test.ts
+++ day2/test.ts
@@ -1,9 +1,14 @@
 import { expect, describe } from '@jest/globals';
-import { resultOutput } from './day2';
+import { resultOutput as firstPuzzleResult } from './day2';
+import { resultOutput as finalPuzzleResult } from './day2-2star';
 
 describe('Test for advent of code day 2', () => {
-  test('Expect given test input to output 8', () => {
+  test('Expect given test input to output 8 in first puzzle', () => {
     expect.assertions(1);
-    return expect(resultOutput('test.txt')).resolves.toBe(8);
+    return expect(firstPuzzleResult('test.txt')).resolves.toBe(8);
+  });
+  test('Expect given test input to output 8 in last` puzzle', () => {
+    expect.assertions(1);
+    return expect(finalPuzzleResult('test.txt')).resolves.toBe(2286);
   });
 });
