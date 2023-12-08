360f63980d573aeb09248fa13edaa29e86dc0fcb
Sajon5271 <sajon5271@gmail.com>
2023-12-08 18:12:06 +0600

    Working on a solution for 2 stars, parsing not working

--- day8/day8-2star.ts
+++ day8/day8-2star.ts
@@ -1,45 +1,59 @@
 import { readFileSync } from 'node:fs';
 
-export async function resultOutput(fileName: string): Promise<number> {
-  return new Promise((resolve, reject) => {
-    const textFile = readFileSync(__dirname + '\\' + fileName, 'utf-8');
-    const splitData = textFile.split('\n\n');
-    type dirString = `/^[LR]+$/`;
-    const direction: dirString = splitData[0] as dirString;
-    const allNodes = splitData[1]
-      .split('\n')
-      .map((el) => {
-        const [key, node] = el.split('=').map((str) => str.trim());
-        const [L, R] = node
-          .substring(1, node.length - 1)
-          .split(',')
-          .map((str) => str.trim());
-        return {
-          [key]: { L, R },
-        };
-      })
-      .reduce((acc, curr) => {
-        return { ...acc, ...curr };
-      }, {});
-    type keyOfNode = keyof typeof allNodes;
-    let startingNodes: keyOfNode[] = Object.keys(allNodes).filter((el) =>
-      el.split('').at(-1)
-    );
-    console.log(startingNodes);
-    // let steps = 0;
-    // const dirLength = direction.length;
-    // while (currPoint !== 'ZZZ') {
-    //   const currDir: 'L' | 'R' = direction[steps % direction.length] as
-    //     | 'L'
-    //     | 'R';
-    //   currPoint = allNodes[currPoint][currDir];
-    //   steps++;
-    // }
-    resolve(0);
-  });
+export function resultOutput(fileName: string): number {
+  const textFile = readFileSync(__dirname + '\\' + fileName, 'utf-8');
+  const splitData = textFile.split('\n\n');
+  type dirString = `/^[LR]+$/`;
+  const direction: dirString = splitData[0] as dirString;
+  const allNodes: { [key: string]: { L: string; R: string } } = splitData[1]
+    .split('\n')
+    .map((el) => {
+      const [key, node] = el.split('=').map((str) =>
+        str
+          .split('')
+          .filter((x) => x)
+          .join()
+      );
+      const [L, R] = node
+        .substring(1, node.length - 1)
+        .split(',')
+        .map((str) =>
+          str
+            .split('')
+            .filter((x) => x)
+            .join()
+        );
+      console.log(L, R, node);
+      return {
+        [key]: { L, R },
+      };
+    })
+    .reduce((acc, curr) => {
+      return { ...acc, ...curr };
+    }, {});
+  console.log(allNodes);
+  let startingNodes = Object.keys(allNodes).filter(
+    (el) => el.split('').at(-1) === 'A'
+  );
+  let steps = 0;
+  console.log(startingNodes, startingNodes.length);
+  const dirLength = direction.length;
+  // while (!checkIfAllEndingNodesEndInZ(startingNodes)) {
+  //   const currDir: 'L' | 'R' = direction[steps % direction.length] as 'L' | 'R';
+  //   startingNodes = startingNodes.map((el) => {
+  //     return allNodes[el][currDir];
+  //   });
+  //   steps++;
+  // }
+  console.log(startingNodes);
+  return steps;
 }
 
-(async function () {
-  const res = await resultOutput('test-2star.txt');
-  console.log(res);
-})();
+// (async function () {
+//   const res = await resultOutput('input-day8.txt');
+//   console.log(res);
+// })();
+console.log(resultOutput('test-2star.txt'));
+function checkIfAllEndingNodesEndInZ(point: string[]): boolean {
+  return point.every((el) => el.split('').at(-1) === 'Z');
+}
